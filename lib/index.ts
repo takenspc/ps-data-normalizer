e36c8473dce5b50901457b6e14b125fde0dbf9b4
Takeshi Kurosawa <taken.spc@gmail.com>
2016-02-29 22:27:28 +0900

    Add statusURL field to StatusEntry

--- lib/index.ts
+++ lib/index.ts
@@ -29,6 +29,7 @@ export class StatusEntry {
     id: string
     title: string
     url: string
+    statusURL: string
     status: Status
 
     constructor(engine: string, id: string, title: string, url: string, status: Status) {
@@ -36,6 +37,27 @@ export class StatusEntry {
         this.id = id;
         this.title = title;
         this.url = url;
+        this.statusURL = StatusEntry.resolveStatusURL(engine, id);
         this.status = status;
+    }
+
+    static resolveStatusURL(engine: string, id: string): string {
+        if (engine === 'chromium') {
+            return 'https://www.chromestatus.com/features/' + encodeURI(id);
+        }
+        
+        if (engine === 'edge') {
+            return 'https://dev.windows.com/en-us/microsoft-edge/platform/status/' + encodeURI(id);
+        }
+        
+        if (engine === 'gecko') {
+            return 'https://platform-status.mozilla.org/#' + encodeURI(id);
+        }
+
+        if (engine === 'webkit') {
+            return 'https://webkit.org/status/#feature-' + encodeURI(id);
+        }
+
+        throw new Error('Unknown engine: ' + engine);
     }
 }
