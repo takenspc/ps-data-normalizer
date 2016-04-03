ec83d705855d2dcecdda1a06c3d996535dfefeb2
Takeshi Kurosawa <taken.spc@gmail.com>
2016-04-03 23:16:21 +0900

    Reduce async

--- lib/updater.ts
+++ lib/updater.ts
@@ -5,8 +5,8 @@ import * as gecko from './gecko/updater';
 import * as webkit from './webkit/updater';
 
 
-export async function update(): Promise<void> {
-    await Promise.all([
+export function update(): Promise<any[]> {
+    return Promise.all([
         chromium.update(),
         edge.update(),
         gecko.update(),
