a9d801e1778258a86a1ac9c6fbcde16ce1d00ccf
Takeshi Kurosawa <taken.spc@gmail.com>
2016-04-03 23:14:41 +0900

    Update url of edge platform status data

--- lib/edge/updater.ts
+++ lib/edge/updater.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import { download } from '../utils';
 
 export function update(): Promise<void> {
-    const url = 'https://dev.windows.com/en-us/microsoft-edge/api/platform/status/';
+    const url = 'https://developer.microsoft.com/en-us/microsoft-edge/api/platform/status/';
     const outPath = path.join(__dirname, 'status.json');
     return download(url, outPath);
 }
