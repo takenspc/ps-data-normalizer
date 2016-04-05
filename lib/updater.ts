37c23973ac8c61bbbbac28ff4b96567f214338c5
Takeshi Kurosawa <taken.spc@gmail.com>
2016-04-05 21:40:10 +0900

    Make updater update ps-url-normalizer

--- lib/updater.ts
+++ lib/updater.ts
@@ -3,10 +3,12 @@ import * as chromium from './chromium/updater';
 import * as edge from './edge/updater';
 import * as gecko from './gecko/updater';
 import * as webkit from './webkit/updater';
+import * as normalizer from 'ps-url-normalizer';
 
 
 export function update(): Promise<any> {
     return Promise.all([
+        normalizer.update(),
         chromium.update(),
         edge.update(),
         gecko.update(),
